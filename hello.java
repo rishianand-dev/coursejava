3b58a0d2b45b94eb08ae21aae1d11ee1f03e3d07
Rishi Anand <rishi.a@honebi.com>
2026-05-17 21:24:30 +0530

    first commit

---
+++ hello.java
@@ -0,0 +1,15 @@
+class Hello
+{
+    public static void main(String a[])
+    {
+        byte x = 10;
+        short y = 20;
+        long z = 30;
+        int b = 5;
+        double c = 6.8;
+        double result = b+c;
+        System.out.println("Hello, World!");
+        System.out.println(result);
+
+    }
+}
